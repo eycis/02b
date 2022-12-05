9fabb7c2ab288e2565e9a5daaa7217c1dfbe78b3
eycis <dzugam00@jcu.cz>
2022-12-05 01:02:54 +0100

    final commit

--- Program.cs
+++ Program.cs
@@ -13,6 +13,15 @@ using (StreamReader streamReader = new StreamReader(@"C:\Users\marie\Downloads\C
        
         List<Transaction> sales = getSales(records);
         List<Transaction> purchases = getPurchases(records);
+        foreach (var item in purchases)
+        {
+            Console.WriteLine($"{item.Id},{item.transaction},{item.Price},{item.Units},{item.DateofTransaction}");
+        }
+        Console.WriteLine("-------------------------------------");
+        foreach (var item in sales)
+        {
+            Console.WriteLine($"{item.Id},{item.transaction},{item.Price},{item.Units},{item.DateofTransaction}");
+        }
 
         foreach (Transaction sell in sales)
         {
@@ -26,8 +35,10 @@ using (StreamReader streamReader = new StreamReader(@"C:\Users\marie\Downloads\C
 
 void getFifo(List<Transaction> purchases, int units, int price, List<Transaction> records, Transaction sell)
 {
+    
     foreach (Transaction purchase in purchases)
     {
+     
         if (purchase.Id > sell.Id)
         {
             return;
@@ -38,15 +49,21 @@ void getFifo(List<Transaction> purchases, int units, int price, List<Transaction
         {
             units -= purchase.Units;
             purchase.Units = 0;
-        } 
+        }
         //kdyby tady bylo and možná? 
         if (units != 0)
         {
             purchases = (from record in records
-                         where (record.transaction == "Purchase" && record.Id > purchase.Id
-                         && record.Id < sell.Id && record.Units != 0)
-                         select record).ToList();
-            //tady by bylo lepší mít podmínku, aby se případně rovnou neprováděla následující funkce
+                            where (record.transaction == "Purchase" && record.Id > purchase.Id
+                            && record.Id < sell.Id && record.Units != 0)
+                            select record).ToList();
+            Console.WriteLine("--------------------------");
+            foreach (var item in purchases)
+            {
+                Console.WriteLine($"{item.Id},{item.transaction},{item.Price},{item.Units},{item.DateofTransaction}");
+            }
+
+            
 
             Console.WriteLine((getAnotherStock(purchases, units, price, CGL)));
         }
@@ -54,9 +71,10 @@ void getFifo(List<Transaction> purchases, int units, int price, List<Transaction
         {
             Console.WriteLine(CGL);
         }
-        
+
     }
-}
+    }
+
 
 int getAnotherStock(IEnumerable<Transaction> purchases, int units, int price, int CGL)
 {
@@ -70,7 +88,9 @@ int getAnotherStock(IEnumerable<Transaction> purchases, int units, int price, in
             units = 0;
             purchase.Units -= originunits;
             CGL += purchase.Units * (price - purchase.Price);
+            break;
         }
+        
     }
     //tady
     return CGL;
@@ -91,33 +111,3 @@ List<Transaction> getSales(List<Transaction> records)
 }
 
 
-
-//omezit nesting, debuggovat. 
-
-//foreach (var item in purchases)
-//{
-//    Console.WriteLine($"{item.Id},{item.transaction},{item.Price},{item.Units},{item.DateofTransaction}");
-//}
-//purchases = (from record in records
-//            where (record.transaction == "Purchase" && record.Id > purchase.Id && record.Id <sell.Id)
-//            select record);
-//není lepší ptát se na to, které nemají x.units =0? 
-
-//Console.WriteLine("sales");
-//foreach (var item in sales)
-//{
-//    Console.WriteLine($"{item.Id},{item.transaction},{item.Price},{item.Units},{item.DateofTransaction}");
-//}
-//else
-//{
-//    Console.WriteLine(CGL);
-//}
-//foreach (var item in purchases)
-//{
-//    Console.WriteLine($"{item.Id},{item.transaction},{item.Price},{item.Units},{item.DateofTransaction}");
-//}
-//je chybe někde tady od 40. řádku. 
-//ono to totiž nemusí být foreach purchase, ale odečítá se do té doby, dokud není units nula.
-//přidat podmínku v případě že units nejsou větší nebo rovno purchase.units
-//tady jde dát metoda s argumenty units a puchase units pro odečítání.
-// problém je na lince 22, kód bere poslední záznam, protože on má stále 50.
